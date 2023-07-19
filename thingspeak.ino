197011021a9fa6738f5f99d67fc5fb5cd90bc8c5
chandini321 <109236098+chandini321@users.noreply.github.com>
2023-07-19 23:32:22 +0530

    Add files via upload
    data visualization code like humidity values per 20seconds same as temperature . It can correlate any two fields using matplotlib visualization

---
+++ thingspeak.ino
@@ -0,0 +1,212 @@
+//----------------------------------------Include the NodeMCU ESP8266 Library
+//----------------------------------------see here: https://www.youtube.com/watch?v=8jMr94B8iN0 to add NodeMCU ESP12E ESP8266 library and board (ESP8266 Core SDK)
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ThingSpeak.h>
+//----------------------------------------
+//----------------------------------------Include the DHT Library
+#include "DHT.h"
+//----------------------------------------
+
+#define DHTTYPE DHT11 //--> Defines the type of DHT sensor used (DHT11, DHT21, and DHT22), in this project the sensor used is DHT11. 
+int WET=14; //D5=GPIO14
+int DRY=12;  //D6=GPIO12
+int sense_Pin=0;
+int value=0;
+//int vcc = 16;
+ 
+const int DHTPin = 5; //--> The pin used for the DHT11 sensor is Pin D1 = GPIO5
+DHT dht(DHTPin, DHTTYPE); //--> Initialize DHT sensor, DHT dht(Pin_used, Type_of_DHT_Sensor);
+
+#define ON_Board_LED 2  //--> Defining an On Board LED, used for indicators when the process of connecting to a wifi router
+
+//----------------------------------------SSID and Password of your WiFi router.
+const char* ssid = "bhanu"; //--> Your wifi name or SSID.
+const char* password = "12345678910"; //--> Your wifi password.
+
+unsigned long mychannelNumber = 2219411;
+String apiKey="4HU8KRL61EKDV36E";
+const char* server = "api.thingspeak.com";
+//----------------------------------------
+
+//----------------------------------------Host & httpsPort
+const char* host = "script.google.com";
+const int httpsPort = 443;
+//----------------------------------------
+
+WiFiClientSecure client; //--> Create a WiFiClientSecure object.
+
+String GAS_ID = "AKfycbxdkbojel0QrcWbzco2al-Tt0VpUQylrT6gk88r30ts3RWOQlqbtduFZMIbUn7YiWs1TA"; //--> spreadsheet script ID
+
+//============================================================================== void setup
+void setup() {
+  // put your setup code here, to run once:
+   //pinMode(vcc,OUTPUT);
+    //digitalWrite(vcc, HIGH);
+  Serial.begin(115200);
+  delay(500);
+
+  pinMode(WET,OUTPUT);
+  pinMode(DRY,OUTPUT);
+
+  delay(500);
+
+  dht.begin();
+  delay(500);
+  
+  WiFi.begin(ssid, password); //--> Connect to your WiFi router
+  Serial.println("");
+    
+  pinMode(ON_Board_LED,OUTPUT); //--> On Board LED port Direction output
+  digitalWrite(ON_Board_LED, HIGH); //--> Turn off Led On Board
+
+  //----------------------------------------Wait for connection
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    //----------------------------------------Make the On Board Flashing LED on the process of connecting to the wifi router.
+    digitalWrite(ON_Board_LED, LOW);
+    delay(250);
+    digitalWrite(ON_Board_LED, HIGH);
+    delay(250);
+    //----------------------------------------
+  }
+  //----------------------------------------
+  digitalWrite(ON_Board_LED, HIGH); //--> Turn off the On Board LED when it is connected to the wifi router.
+  //----------------------------------------If successfully connected to the wifi router, the IP Address that will be visited is displayed in the serial monitor
+  Serial.println("");
+  Serial.print("Successfully connected to : ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  //----------------------------------------
+
+  client.setInsecure();
+}
+//==============================================================================
+//============================================================================== void loop
+void loop() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  int h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  if (client.connect(server,80)) {
+String postStr = apiKey;
+postStr +="&field1=";
+postStr += String(t);
+postStr +="&field2=";
+postStr += String(h);
+postStr += "\r\n\r\n";
+ 
+client.print("POST /update HTTP/1.1\n");
+client.print("Host: api.thingspeak.com\n");
+client.print("Connection: close\n");
+//client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+client.print("Content-Type: application/x-www-form-urlencoded\n");
+client.print("Content-Length: ");
+client.print(postStr.length());
+client.print("\n\n");
+client.print(postStr);
+ 
+Serial.print("Temperature: ");
+Serial.print(t);
+Serial.print(" degrees Celsius Humidity: ");
+Serial.print(h);
+Serial.println("Sending data to Thingspeak");
+}
+client.stop();
+ 
+Serial.println("Waiting 20 secs");
+// thingspeak needs at least a 15 sec delay between updates
+// 20 seconds to be safe
+delay(20000);
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor !");
+    delay(500);
+    return;
+  }
+  String Temp = "Temperature : " + String(t) + " °C";
+  String Humi = "Humidity : " + String(h) + " %";
+  Serial.println(Temp);
+  Serial.println(Humi);
+  
+  sendData(t, h); //--> Calls the sendData Subroutine
+}
+//==============================================================================
+//============================================================================== void sendData
+// Subroutine for sending data to Google Sheets
+void sendData(float tem, int hum) {
+  Serial.println("==========");
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  //----------------------------------------Connect to Google host
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  //----------------------------------------
+
+  //----------------------------------------Processing data and sending data
+  String string_temperature =  String(tem);
+  // String string_temperature =  String(tem, DEC); 
+  String string_humidity =  String(hum, DEC); 
+  String url = "/macros/s/" + GAS_ID + "/exec?temperature=" + string_temperature + "&humidity=" + string_humidity;
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+         "Host: " + host + "\r\n" +
+         "User-Agent: BuildFailureDetectorESP8266\r\n" +
+         "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  //----------------------------------------
+
+  //----------------------------------------Checking whether the data was sent successfully or not
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.print("reply was : ");
+  Serial.println(line);
+  Serial.println("closing connection");
+  Serial.println("==========");
+  Serial.println();
+  //----------------------------------------
+  delay(100);
+  Serial.print("MOISTURE LEVEL : ");
+   value= analogRead(sense_Pin);
+   value= value/10;
+   Serial.println(value);
+   if(value>70)
+   {
+      digitalWrite(WET,LOW);
+      digitalWrite(DRY,HIGH);
+      
+   }
+   else
+   {
+      digitalWrite(WET,HIGH);
+      digitalWrite(DRY,LOW);
+      
+   }
+   delay(200);
+
+  
+} 
+//==============================================================================
+
